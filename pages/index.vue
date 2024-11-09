48d62c436424247a62826665221c009c938240e0
meowjestic <aldodoohan13@gmail.com>
2024-11-09 23:30:39 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,8 @@
     <header class="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
         <nav
             class="mt-4 relative max-w-3xl w-full bg-white border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto  ">
-            <div class="px-4 md:px-0 flex justify-between items-center">
+            <!-- <div class="px-4 md:px-0 flex justify-between items-center">
                 <div class="md:hidden">
-                    <!-- Toggle Button -->
                     <button type="button"
                         class="hs-collapse-toggle flex justify-center items-center size-6 border border-gray-200 text-gray-500 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
                         id="hs-navbar-header-floating-collapse" aria-expanded="false"
@@ -25,12 +24,11 @@
                             <path d="m6 6 12 12" />
                         </svg>
                     </button>
-                    <!-- End Toggle Button -->
                 </div>
-            </div>
+            </div> -->
 
             <div id="hs-navbar-header-floating"
-                class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
+                class="block hs-collapse overflow-hidden transition-all duration-300 basis-full grow"
                 aria-labelledby="hs-navbar-header-floating-collapse">
                 <div
                     class="flex flex-col md:flex-row md:items-center md:justify-center gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
